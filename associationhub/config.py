d1285a8bafe7e3985444f74e9eaf0bcb44013fdd
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-06-04 22:08:47 +0100

    Delete associationhub directory

--- associationhub/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://associationhub_user:password@localhost/associationhub'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
--- associationhub/key.py
+++
@@ -1,6 +0,0 @@
-import os
-
-# Générer une clé secrète aléatoire
-secret_key = os.urandom(24)
-print(secret_key.hex())
-
