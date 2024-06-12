c7e58f5ff35e2e09281d2130f333f413c819553f
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-06-12 01:06:42 +0100

    Update routes.py

--- association/association_v1/app/routes.py
+++ association/association_v1/app/routes.py
@@ -1,3 +1,4 @@
+from sqlalchemy import func
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from .models import db, User, Association, News, Event, Report, Achievement, Media
