357905158b8597971b3fc89384f157dd083711c9
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-06-13 21:34:42 +0100

    Update models.py

--- association/association_v1/app/models.py
+++ association/association_v1/app/models.py
@@ -7,7 +7,6 @@ db = SQLAlchemy()
 
 class User(db.Model, UserMixin):
     __tablename__ = 'user'
-
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(100), nullable=False)
