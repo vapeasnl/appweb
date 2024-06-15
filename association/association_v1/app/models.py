5589dd669e189430c1eb6c90469c45f1a2e5404e
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-06-16 00:44:07 +0100

    Update models.py

--- association/association_v1/app/models.py
+++ association/association_v1/app/models.py
@@ -1,9 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from datetime import datetime
-from . import login_manager
+from . import db, login_manager
 
-db = SQLAlchemy()
 
 class User(db.Model, UserMixin):
     __tablename__ = 'user'
