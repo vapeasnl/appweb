b24904848570561f44f47cadd64b27403d5428dd
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-06-20 19:46:05 +0100

    Update routes.py

--- association/association_v1/app/routes.py
+++ association/association_v1/app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask import Blueprint, render_template, request, redirect, url_for, flash, g
 from flask_login import login_user, logout_user, login_required, current_user
 from .models import db, User, Association, News, Event, Report, Achievement, Media, ContactMessage
 from datetime import datetime
