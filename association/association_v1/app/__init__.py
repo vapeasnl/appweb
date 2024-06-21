7ffee969f6c2b6fc3b4001a79a1e8c033b1eb2dc
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-06-21 23:28:55 +0100

    Update __init__.py

--- association/association_v1/app/__init__.py
+++ association/association_v1/app/__init__.py
@@ -11,6 +11,11 @@ login_manager = LoginManager()
 def create_app():
     app = Flask(__name__)
     app.config.from_object('app.config.Config')
+    app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'static', 'uploads')
+    
+    # Créez le dossier de téléchargement s'il n'existe pas
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
     
     db.init_app(app)
     migrate.init_app(app, db)
@@ -24,6 +29,5 @@ def create_app():
     app.register_blueprint(profile_bp)
     app.register_blueprint(news_bp)
 
-    app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'static', 'uploads')
     return app
 
