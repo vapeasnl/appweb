327439d6846a27ce4d97920fc17823e3a257b1cc
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-06-22 01:33:50 +0100

    Update __init__.py

--- association/association_v1/app/__init__.py
+++ association/association_v1/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager, UserMixin
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
