34f8f7ac0ed9c2166ef71c3d979b5448e8c67751
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-06-22 01:20:34 +0100

    Update routes.py

--- association/association_v1/app/routes.py
+++ association/association_v1/app/routes.py
@@ -247,12 +247,13 @@ def dashboard():
     achievements = Achievement.query.paginate(page=page_achievements, per_page=10)
     media_list = Media.query.paginate(page=page_media, per_page=10)
 
-    # Récupérer les noms des événements et les comptes de présences
-    event_names = [event.name for event in events.items]
-    attendance_counts = [event.attendances.count() for event in events.items]
+    # Get attendances
+    attendances = Attendance.query.all()  # Assuming you have an Attendance model
 
-    # Récupérer toutes les présences
-    all_attendances = Attendance.query.all()
+    print(attendances)  # Add this line to check if attendances are fetched correctly
+
+    event_names = [event.name for event in events.items]
+    attendance_counts = [len(event.attendances) for event in events.items]
 
     return render_template('dashboard.html', 
                            reports=reports, 
@@ -263,7 +264,7 @@ def dashboard():
                            media_list=media_list, 
                            event_names=event_names, 
                            attendance_counts=attendance_counts, 
-                           all_attendances=all_attendances,
+                           attendances=attendances,
                            unread_count=g.unread_count)
 
 # Report routes
